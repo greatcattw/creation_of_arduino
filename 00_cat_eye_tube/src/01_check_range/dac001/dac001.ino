274b0992167756c92446a0cae76a7fe211935239
greatcattw <35946390+greatcattw@users.noreply.github.com>
2025-08-07 10:54:29 +0800

    Add files via upload

---
+++ 00_cat_eye_tube/src/01_check_range/dac001/dac001.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+Adafruit_MCP4725 dac;
+
+void setup() {
+  // put your setup code here, to run once:
+  dac.begin(0x60);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dac.setVoltage(0, false);
+  delay(2000);
+  dac.setVoltage(4095, false);
+  delay(2000);
+}
